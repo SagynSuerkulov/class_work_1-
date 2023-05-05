4c3d44f67054e2eac6cc357b5f856627e3a9dd08
SagynSuerkulov <english290998sagyn@gmail.com>
2023-05-05 21:47:59 +0600

    first commit

---
+++ bin/lesson_4s.dart
@@ -0,0 +1,57 @@
+//void main()
+//task 1
+//  List userScore = [1, 7, 12, 3, 56, 2, 87, 34, 54];
+//print('${userScore.last}{\\ ${userScore[4]}\\ ${userScore.last}');
+
+//task 2
+//List one = [3, 12, 43, 1, 25, 6, 5, 7];
+//List two = [55, 11, 23, 56, 78, 1, 9];
+//one.addAll(two);
+//one.sort();
+//print(one.reversed.toList());
+//task 3
+//List list ['a','d','F','l','L','u','t','e','r'];
+//print (list.getRange(2, 9))
+//print (list.getRange(2, 9))
+
+// task 4
+//  List a = ['post', 1, 0, 'flutter'];
+//  String myDart = 'flutter';
+//  print(a.contains(myDart.toLowerCase()));
+//}
+void main() {
+  // int a = 10;
+  // int b = 1;
+  // if (a > b) {
+  //   print('a больше чем b');
+  // } else {
+  //   print('a >больше чем b');
+  // }
+  int a = 100;
+
+  // if (a > 9 && a < 100) {
+  //   print('число двузначное');
+  // } else {
+  //   print('Число не двузначное ');
+  // }
+
+  //int a = 1000;
+// if (a > 9 || a < 100) {
+//  print('число двузначное');
+// } else {
+//    print('Число не двузначное ');
+  //
+  // int a = 100;
+  // if (a == 1000) {
+  //   print('да');
+  // } else {
+  //   print('нет');
+  // }
+  // if (a == 1000) {
+  //   print('равен');
+  // } else {
+  //   print('Не равен');
+  // }
+  // a == 1000 ? print('равен') : print('Не равен');
+  
+}
---
+++ test/lesson_4s_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_4s/lesson_4s.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
